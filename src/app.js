08c5c051d9ce743e2894ca7ad6ecc67df071f22a
joedillonuk <joe.dillon.uk@gmail.com>
2020-06-16 16:44:04 +0100

    Initial setup. File heiarchy in place and method mounted to retrieve currencies from API.

---
+++ src/app.js
@@ -0,0 +1,23 @@
+import Vue from 'vue';
+
+document.addEventListener("DOMContentLoaded", () => {
+  new Vue({
+    el: "#app",
+    data: {
+      AllCurrencies: {},
+    },
+    computed: {
+
+    },
+    mounted(){
+      this.getCurrencies()
+    },
+    methods: {
+      getCurrencies: function(){
+        fetch("https://api.exchangeratesapi.io/latest")
+        .then(res => res.json())
+        .then(currencies => this.AllCurrencies = currencies)
+      },
+    }
+  })
+})
